3cf6a140d26967b81be2054a34c914745dfc2044
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-06-25 13:54:09 +0200

    test

--- src/index.js
+++ src/index.js
@@ -4,46 +4,72 @@ import Notiflix from 'notiflix';
 form.addEventListener("submit", handleSubmit);
 const gallery = document.querySelector(".gallery");
 let markup = "";
-const API_KEY ='28203095-60f45d0309e92efa731dcf20a';
+const API_KEY = '28203095-60f45d0309e92efa731dcf20a';
+let pageNr = 1;
+const loadMore = document.querySelector(".load-more");
+loadMore.style.visibility = "hidden";
+
 
 function handleSubmit(event) {
   event.preventDefault();
   const {
     elements: { searchQuery, button }
   } = event.currentTarget;
-  const searchParams = new URLSearchParams(
-    {
-      key: API_KEY,
-      q: searchQuery.value,
-      image_type: "photo",
-      orientation: "horizontal",
-      safesearch: "true",
-    }
-  );  
+  pageNr = 1;
   async function getPictures() {
-    const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
+    const searchParams = new URLSearchParams(
+      {
+        key: API_KEY,
+        q: searchQuery.value,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: "true",
+        per_page: 10,
+        page: pageNr,
+      }
+    );  
+    let response = await axios({
+      method: 'get',
+      url: `https://pixabay.com/api/?${searchParams}`,
+    });
     const data = response.data;
     return data
   };
   console.log(searchQuery.value);
 
-  getPictures().then(data => renderData(data)).catch(error=>console.log(error));
+  getPictures()
+    .then(data => renderData(data))
+    .catch(error => console.log(error));
+  
+  loadMore.addEventListener("click", loadMoreImg);
+  function loadMoreImg(event) {
+    event.preventDefault();
+    pageNr += 1;
+    loadMore.style.visibility = "hidden";
+    console.log(`page: ${pageNr}`);
+      getPictures()
+      .then(data => renderData(data))
+      .catch(error => console.log(error));
+  }
+  
 }
 
 function renderData(data) {
-  console.log(data);
-  console.log(data.total);
-  console.log(data.totalHits);
-  console.log(data.hits);
+  // console.log(data);
+  // console.log(data.total);
+  // console.log(data.totalHits);
+  console.log(data.hits[0]);
   if (data.total === 0) {
     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
     markup = "";
     gallery.innerHTML = markup;
+    loadMore.style.visibility = "hidden";
   } else {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
       markup = data.hits
         .map((hit) => {
           return `<div class="photo-card">
-            <img class="photo" src="${hit.previewURL}" alt="${hit.tags}" loading="lazy" />
+            <img class="photo" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
             <div class="info">
               <p class="info-item">
                 <b>Likes ${hit.likes}</b>
@@ -61,10 +87,13 @@ function renderData(data) {
           </div>`;
         })
       .join(""); 
-     gallery.innerHTML = markup;
+    gallery.insertAdjacentHTML('beforeend', markup);
+    loadMore.style.visibility = "visible";
   };
- 
-}
+  
+
+};
+
 
 // const axios = require('axios');
 
