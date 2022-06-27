0699541933bf2628ca3ada083ad5c2dbc9e9edf5
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-06-27 16:44:05 +0200

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const form = document.querySelector('#search-form');
 import Notiflix from 'notiflix';
 form.addEventListener("submit", handleSubmit);
 let gallery = document.querySelector(".gallery");
-// let markup = "";
 let lightBox = new SimpleLightbox('.gallery a');
 
 const loadMore = document.querySelector(".load-more");
@@ -21,7 +20,6 @@ function handleSubmit(event) {
   searchQueryValue = searchQuery.value
   pageNr = 1;
   gallery.innerHTML = "";
-  console.log(searchQuery.value);
   if (searchQueryValue.trim() === '') {
     Notiflix.Notify.failure("Please insert proper serach query");
     loadMore.style.visibility = "hidden";
@@ -34,7 +32,6 @@ function handleSubmit(event) {
 }
 
 function renderData(data) {
-  console.log(data.hits[0]);
   if (data.total === 0) {
     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
     markup = "";
@@ -44,35 +41,6 @@ function renderData(data) {
     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
     loadPhotos(data);
     loadMore.style.visibility = "visible";
-    
-      // markup = data.hits
-      //   .map((hit) => {
-      //     return `<a href="${hit.largeImageURL}"><div class="photo-card">
-      //       <img class="photo" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
-      //       <div class="info">
-      //         <p class="info-item">
-      //           <b>Likes ${hit.likes}</b>
-      //         </p>
-      //         <p class="info-item">
-      //           <b>Views ${hit.views}</b>
-      //         </p>
-      //         <p class="info-item">
-      //           <b>Comments ${hit.comments}</b>
-      //         </p>
-      //         <p class="info-item">
-      //           <b>Downloads ${hit.downloads}</b>
-      //         </p>
-      //       </div>
-      //     </div></a>`;
-      //   })
-      // .join(""); 
-    // gallery.insertAdjacentHTML('beforeend', markup);
-    
-    // let box = gallery;
-    // box = new SimpleLightbox('.gallery a');
-    // let gallerySimpleLightBox = gallery;
-    // gallerySimpleLightBox = new SimpleLightbox('.gallery a');
-    
   };
 };
 
@@ -99,7 +67,7 @@ function loadPhotos(data) {
       })
     .join(""); 
   if (pageNr !== 1) {
-      checkTotalHits(data.totalHits, pageNr);
+    checkTotalHits(data.totalHits, pageNr);
   }
   pageNr += 1;
   loadMore.style.visibility = "visible";
@@ -107,6 +75,9 @@ function loadPhotos(data) {
 
   gallery.insertAdjacentHTML('beforeend', markup);
   lightBox.refresh();
+    if (pageNr !== 2) {
+    scroll();
+  }
 };
   
 loadMore.addEventListener("click", loadMoreImg);
@@ -114,7 +85,6 @@ loadMore.addEventListener("click", loadMoreImg);
 function loadMoreImg(event) {
   event.preventDefault();
   loadMore.style.visibility = "hidden";
-  console.log(`page: ${pageNr}`);
     getPictures(searchQueryValue, pageNr)
     .then(data => loadPhotos(data))
     .catch(error => console.log(error));
@@ -124,4 +94,14 @@ function checkTotalHits(totalHits, pageNr) {
   if (totalHits - pageNr * 40 < 0) {
     Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
   };  
+};
+function scroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
